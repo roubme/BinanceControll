5b709846bcf64703b96c855c8a75bfd7ea985a87
gyejinpark <roub.cto@gmail.com>
2021-09-29 23:32:17 +0900

    Binance_order_test.pytest.py Using Hedge Mode

--- Binance_order_test.py
+++ Binance_order_test.py
@@ -1,19 +1,72 @@
-import ccxt
 import pprint
-import time
+
+from binance.client import Client
 import pandas as pd
 
-api_key = "0CtoAOdfUKM6UMpe4HbNJq5MQwNDzsdKMV9jbImdmFjZBmCO9RwCskT3dXgrYTNA"
-secret = "VcdyMBBtjrCxdhkU1xR4INLaYFMJbm1zm9zWW5mTVgeDmFDQ8T3m4LAUD7dD0NXu"
+user_key = 'qT5UcrVe4bOgVOX3QREn7wuFPlaqsh4K8ygOF4MkL9J0e23xmQu5D4LBDJPN27pX'
+secret_key = 'pr2zMpAWMXkUy7Y03o1VocvpTgYGkax55GLopRmCrfTxZP6L407GHJqa7g9qzUYq'
 
-binance = ccxt.binance(config={
-    'apiKey': api_key,
-    'secret': secret,
-    'enableRateLimit': True,
-    'options': {
-        'defaultType': 'future'
-    }
-})
+binance_client = Client(user_key, secret_key)
 
-order = binance.create_market_buy_order("DOT/USDT", 0.5)
-print(order)
+# MARKET DATA
+# current futures price
+binance_client.futures_coin_symbol_ticker(symbol='BTCUSD_PERP')
+
+# OHLCV historical
+df = df = pd.DataFrame(binance_client.futures_historical_klines(
+    symbol='BTCUSDT',
+    interval='1d',  # can play with this e.g. '1h', '4h', '1w', etc.
+    start_str='2021-06-01',
+    end_str='2021-06-30'
+))
+
+# crop unnecessary columns
+df = df.iloc[:, :6]
+
+# ascribe names to columns
+df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+
+# convert timestamp to date format and ensure ohlcv are all numeric
+df['date'] = pd.to_datetime(df['date'], unit='ms')
+for col in df.columns[1:]:
+    df[col] = pd.to_numeric(df[col])
+
+# TRADING
+# test order
+binance_client.create_test_order(
+    symbol='BTCUSDT',
+    type='MARKET',
+    side='BUY',
+    quantity=0.001
+)
+
+# order book - into a pandas DataFrame for neater output
+df = pd.DataFrame(
+    binance_client.futures_order_book(symbol='BTCUSDT')
+)
+print(df[['bids', 'asks']].head())
+
+# set leverage - IMPORTANT
+leverage = binance_client.futures_change_leverage(symbol='BTCUSDT', leverage=25)
+pprint.pprint(leverage)
+# limit order
+# binance_client.futures_create_order(
+#    symbol='BTCUSDT',
+#    type='LIMIT',
+#    timeInForce='GTC',  # Can be changed - see link to API doc below
+#    price=30000,  # The price at which you wish to buy/sell, float
+#    side='BUY',  # Direction ('BUY' / 'SELL'), string
+#    quantity=0.001  # Number of coins you wish to buy / sell, float
+#)
+
+# market order
+binance_client.futures_create_order(
+    symbol='BTCUSDT',
+    type='MARKET',
+#    timeInForce='GTC',
+    positionSide='LONG', side='SELL',
+    quantity=0.001
+)
+
+# view outstanding orders
+binance_client.futures_get_open_orders(symbol='BTCUSDT')
--- Binance_test.py
+++ Binance_test.py
@@ -1,39 +1,5 @@
 import ccxt
-import pandas as pd
 import pprint
-import time
-import datetime
-
-print('CCXT Version:', ccxt.__version__)
-
-# binance = ccxt.binance()
-# btc_ohlcv = binance.fetch_ohlcv("BTC/USDT")
-#type(btc_ohlcv)
-#print(btc_ohlcv)
-#markets = binance.load_markets()
-#btc = binance.fetch_ticker("BTC/USDT")
-#for market in markets.keys():
-#    if market.endswith("/USDT"):
-#        print(market)
-#pprint.pprint(btc)
-
-#print("현재가", btc['last'])
-
-# df = pd.DataFrame(btc_ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
-# df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
-# df.set_index(df['datetime'], inplace=True)
-
-# symbol = "BTC/USDT"
-
-# while True:
-#    btc = binance.fetch_ticker(symbol)
-#    now = datetime.datetime.now()
-#    print(now, btc['last'])
-#    time.sleep(1)
-
-
-
-
 
 with open("api.txt") as f:
     lines = f.readlines()
@@ -44,65 +10,16 @@ binance = ccxt.binance(config={
     'apiKey': api_key,
     'secret': secret,
     'enableRateLimit': True,
+    'dualSidePosition': True,
     'options': {
         'defaultType': 'future'
     }
 })
 
-order = binance.create_market_buy_order(
-    symbol="DOT/USDT",
-    amount=0.4
+order = binance.create_market_order(
+    symbol="BTC/USDT",
+    side="BUY",
+    amount=0.001
 )
 
 pprint.pprint(order)
-
-
-# params = {
-#    'test': True,  # test if it's valid, but don't actually place it
-#}
-
-# markets = binance.load_markets()
-# markets = binance.load_markets()
-# symbol = "DOT/USDT"
-# market = binance.market(symbol)
-# leverage = 2
-
-# resp = binance.fapiPrivate_post_leverage({
-#    'symbol': market['id'],
-#    'leverage': leverage
-#})
-
-# symbol = symbol,
-# type = 'limit',
-# side = 'sell',
-# amount = 0.1,
-# price = 29.338
-
-# exchange.options = {'defaultType': 'future', 'adjustForTimeDifference': True,'defaultTimeInForce':False}
-# binance.options['defaultType'] = 'future'
-# binance.options['adjustForTimeDifference'] = False
-# binance.options['defaultTimeInForce'] = 'GTC'
-# binance.set_sandbox_mode(enabled=True)
-# binance.set_sandbox_mode(enabled=True)
-# order = binance.create_order(symbol, type, side, amount, price, params)
-#pprint.pprint(order)
-# balance = binance.fetch_balance(params={"type": "future"})
-# btc = binance.fetch_ticker("BTC/USDT")
-# btc_ohlcv = binance.fetch_ohlcv(symbol='BTC/USDT', timeframe='1d', since=None, limit=10)
-# print(balance['USDT'])
-# print(api_key)
-# print(secret)
-# pprint.pprint(balance['total'])
-# pprint.pprint(btc['last'])
-# df = pd.DataFrame(btc_ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
-# df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
-# df.set_index('datetime', inplace=True)
-
-# pprint.pprint(df)
-
-# order_book = binance.fetch_order_book("BTC/USDT")
-# asks = order_book['asks']
-# bids = order_book['bids']
-
-# pprint.pprint(asks[0])
-# pprint.pprint(bids[0])
